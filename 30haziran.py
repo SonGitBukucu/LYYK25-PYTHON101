54e341477d9fc75c6b08a5bfd50d9968fdaa82b4
SonGitBukucu <makaya2620@gmail.com>
2025-06-30 14:37:22 +0300

    14.37

--- 30haziran.py
+++ 30haziran.py
@@ -1,7 +1,6 @@
 isim = input("adınız: ")
-
 soyad = input("soyadınız: ")
-
 yas = int(input("yaşınız: "))
-
 urun = input("ne içmek istersiniz: ")
+
+print("merhaba " + isim + " " + soyad + ". Yaşınız: " + str(yas) + ". İçecek tercihiniz: " + urun + ".")
