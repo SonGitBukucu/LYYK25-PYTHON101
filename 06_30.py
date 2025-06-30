8ec56fb476c170876740389d0039fc1ec8d852b5
SonGitBukucu <makaya2620@gmail.com>
2025-06-30 15:37:14 +0300

    sabit fiyat eklendi

--- 06_30.py
+++ 06_30.py
@@ -2,7 +2,8 @@ isim = input("adınız: ")
 soyad = input("soyadınız: ")
 yas = int(input("yaşınız: "))
 urun = input("ne içmek istersiniz: ")
+tutar =  70
 
 #print("merhaba " + isim + " " + soyad + ". Yaşınız: " + str(yas) + ". İçecek tercihiniz: " + urun + ".")
 
-print(f"Merhaba {isim} {soyad}, yaşınız {yas}. İçecek tercihiniz: {urun}")
+print(f"Merhaba {isim} {soyad}, yaşınız {yas}. İçecek tercihiniz: {urun}. Toplam tutarınız: {tutar}TL")
