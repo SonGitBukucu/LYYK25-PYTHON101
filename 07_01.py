6e90ca936b7eea74372faf1da9a3fb5adf846943
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 13:59:41 +0300

    13.59 üçüncü soru

--- 07_01.py
+++ 07_01.py
@@ -3,7 +3,13 @@
 
 #print(f"{simdikiYil} yılı itibariyle {simdikiYil - dogumYili} yaşındasınız.")
 
-i = 1
-while i < 501 :
-    print(f"Merhabalar! ({i} / 500)")
-    i += 1
+#i = 1
+#while i < 501 :
+#    print(f"Merhabalar! ({i} / 500)")
+#    i += 1
+
+aci1 = int(input("Üçgenin birinci açısı: "))
+aci2 = int(input("Üçgenin ikinci açısı: "))
+aci3 = 180 - (aci1 + aci2)
+
+print(f"üçgende birinci açı {aci1} derece, ikinci açı {aci2} derece ise üçüncü açı {aci3} derecedir.")
