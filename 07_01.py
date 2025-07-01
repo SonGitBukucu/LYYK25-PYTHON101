de49492202f1fa7dd0cca7d88858df908b85bb1c
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 16:50:05 +0300

    16.50

--- 07_01.py
+++ 07_01.py
@@ -5,15 +5,14 @@ biyoloji = int(input("Biyoloji ders notu: "))
 ortalama = (fizik + kimya + biyoloji) / 3
 ortalama = round(ortalama, 2)
 
+print(ortalama)
+
 if ortalama < 0:
     print("Girilen notlar negatif olamaz.")
 elif ortalama > 100:
     print("Girilen notlar 100'den büyük olamaz.")
 else:
     print("Notlar geçerli.")
-
-
-    print(ortalama)
 
     if ortalama < 50:
         print("Sınıfta kaldın.")
