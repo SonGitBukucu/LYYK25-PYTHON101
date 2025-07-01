5af17d657b71bee6eaf71f0def60fdec88b49f07
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 16:15:42 +0300

    16.15

--- 07_01.py
+++ 07_01.py
@@ -1,6 +1,12 @@
-yas = int(input("yaşınızı girin: "))
+#girilen 3 ders notu, ortalama bul, 50 < ise sınıfta kal, büyükse geç
 
-if yas == 18:
-    print("ehliyet alabilirsin")
-else :
-    print("ehliyet alamazsın")
+fizik = int(input("Fizik ders notu: "))
+kimya = int(input("Kimya ders notu: "))
+biyoloji = int(input("biyoloji ders notu: "))
+
+ortalama = (fizik + kimya + biyoloji) / 3
+
+if ortalama < 50:
+    print(f"Ortalaman {ortalama}, sınıfta kaldın.")
+else:
+    print(f"Sınıfı geçtin. Ortalaman {ortalama}.")
