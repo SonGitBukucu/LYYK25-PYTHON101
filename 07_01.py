549d6c2d219d25eda5c1e41f171ea2d8b772b122
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 11:26:34 +0300

    11.26

--- 07_01.py
+++ 07_01.py
@@ -1,7 +1,13 @@
-sayi1 = 6
-sayi2 = 14
-girdi1 = int(input("birinci sayiyi girin: "))
-girdi2 = int(input("ikinci sayıyı girin: "))
-sonuc = sayi1 + sayi2 + girdi1 + girdi2
+cayFiyat = 20
+kahveFiyat = 50
 
-print(f"{sayi1} + {sayi2} + {girdi1} + {girdi2} = {sonuc}")
+caySayi = int(input("Kaç bardak çay istersiniz: "))
+kahveSayi = int(input("Kaç bardak kahve istersiniz: "))
+
+cayToplam = cayFiyat * caySayi
+kahveToplam = kahveFiyat * kahveSayi
+total = cayToplam + kahveToplam
+
+print(f"{caySayi} bardak çay toplam {cayFiyat}*{caySayi} = {cayToplam}TL")
+print(f"{kahveSayi} bardak kahve toplam {kahveFiyat}*{kahveSayi} = {kahveToplam}TL")
+print(f"Toplam tutar: {cayToplam} + {kahveToplam} = {total}TL ")
