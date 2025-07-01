2962c799dc1f4b86c17c427635cdc459b3cc9e69
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 14:07:06 +0300

    düzeltmesel

--- 07_01.py
+++ 07_01.py
@@ -3,13 +3,13 @@
 
 #print(f"{simdikiYil} yılı itibariyle {simdikiYil - dogumYili} yaşındasınız.")
 
-#x = 1
-#while x < 501 :
-#    print(f"Merhabalar! ({x} / 500)")
-#    i += x
+x = 1
+while x < 501 :
+    print(f"{x}. Merhabalar!")
+    x += 1
 
-aci1 = int(input("Üçgenin birinci açısı: "))
-aci2 = int(input("Üçgenin ikinci açısı: "))
-aci3 = 180 - (aci1 + aci2)
+#aci1 = int(input("Üçgenin birinci açısı: "))
+#aci2 = int(input("Üçgenin ikinci açısı: "))
+#aci3 = 180 - (aci1 + aci2)
 
-print(f"üçgende birinci açı {aci1} derece, ikinci açı {aci2} derece ise üçüncü açı {aci3} derecedir.")
+#print(f"üçgende birinci açı {aci1} derece, ikinci açı {aci2} derece ise üçüncü açı {aci3} derecedir.")
