576a649f634fd34f128ab24ce8c3c310d63e17f7
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 13:51:50 +0300

    13.51 birinci soru

--- 07_01.py
+++ 07_01.py
@@ -1,9 +1,4 @@
-mat = int(input("Matematik dersinden kaç aldın? :"))
-ing = int(input("İngilizce dersinden kaç aldın? :"))
-tr = int(input("Türkçe dersinden kaç aldın? :"))
-fiz = int(input("Fizik dersinden kaç aldın? :"))
-biy = int(input("Biyoloji dersinden kaç aldın? :"))
+#dogumYili = int(input("Doğum yılınız: "))
+#simdikiYil = 2025
 
-ortalama = (mat + ing + tr + fiz + biy) / 5
-
-print(f"ortalaman: {ortalama}")
+#print(f"{simdikiYil} yılı itibariyle {simdikiYil - dogumYili} yaşındasınız.")
