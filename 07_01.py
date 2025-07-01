99acc145297daf870cc1bc368cfb283f51dbeaf1
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 11:38:28 +0300

    11.38 (imla)

--- 07_01.py
+++ 07_01.py
@@ -1,9 +1,9 @@
 mat = int(input("Matematik dersinden kaç aldın? :"))
 ing = int(input("İngilizce dersinden kaç aldın? :"))
 tr = int(input("Türkçe dersinden kaç aldın? :"))
-fzk = int(input("Fizik dersinden kaç aldın? :"))
+fiz = int(input("Fizik dersinden kaç aldın? :"))
 biy = int(input("Biyoloji dersinden kaç aldın? :"))
 
-ortalama = (mat + ing + tr + fzk + biy) / 5
+ortalama = (mat + ing + tr + fiz + biy) / 5
 
 print(f"ortalaman: {ortalama}")
