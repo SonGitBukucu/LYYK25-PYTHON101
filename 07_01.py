ab8e99a4504fb45f0f9c91269918291aa728b2da
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 15:09:40 +0300

    15.09

--- 07_01.py
+++ 07_01.py
@@ -14,6 +14,6 @@ tuzluYuzde = tuzlu / toplamYiyecek * 100
 baklavaYuzde = baklava / tatli * 100
 sarmaYuzde = sarma / tuzlu * 100
 
-print(f"tatlı-tuzlu yüzde oranları: {tatliYuzde} tatlı / {tuzluYuzde} tuzlu")
+print(f"tatlı-tuzlu yüzde oranları: {tatliYuzde} tatlı / {tuzluYuzde} tuzlu ({tatli} tatlı / {tuzlu} tuzlu)")
 print(f"tatlılar arasında baklavanın oranı yüzde {baklavaYuzde}")
 print(f"tuzlular arasında sarmanın yüzde oranı yüzde {sarmaYuzde}")
