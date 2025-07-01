745a57b78ddf18a951d932f0ec9d5a374db3e95b
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 10:38:23 +0300

    10.38

--- 07_01.py
+++ 07_01.py
@@ -1,13 +1,5 @@
-isim = input("adınız: ")
-soyad = input("soyadınız: ")
-yas = int(input("yaşınız: "))
-urun = input("ne içmek istersiniz: ")
-urunSayi = int(input("kaç tane içecek istiyorsunuz: "))
-tutar = 70
+isim = "alper"
+sayi = 6
+ondalik = 3.14
 
-#print("merhaba " + isim + " " + soyad + ". Yaşınız: " + str(yas) + ". İçecek tercihiniz: " + urun + ".")
-print(f"Merhaba {isim} {soyad}, yaşınız {yas}. İçecek tercihiniz: {urun}. Toplam tutarınız: {urunSayi}*{tutar} = {tutar*urunSayi}TL")
-
-yasKontrol = yas < 18
-
-print(type(yasKontrol))
+print(type(ondalik))
