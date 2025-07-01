d5f4dfcaad585ab1e32c22dbab8ed059aabe67f3
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 11:36:37 +0300

    11.36

--- 07_01.py
+++ 07_01.py
@@ -1,13 +1,9 @@
-cayFiyat = 20
-kahveFiyat = 50
+mat = int(input("Matematik dersinden kaç aldın? :"))
+ing = int(input("İngilizce dersinden kaç aldın? :"))
+tr = int(input("Türkçe dersinden kaç aldın? :"))
+fzk = int(input("Fizik dersinden kaç aldın? :"))
+biy = int(input("Biyoloji dersinden kaç aldın? :"))
 
-caySayi = int(input("Kaç bardak çay istersiniz: "))
-kahveSayi = int(input("Kaç bardak kahve istersiniz: "))
+ortalama = (mat + ing + tr + fzk + biy) / 5
 
-cayToplam = cayFiyat * caySayi
-kahveToplam = kahveFiyat * kahveSayi
-total = cayToplam + kahveToplam
-
-print(f"{caySayi} bardak çay toplam {cayFiyat}*{caySayi} = {cayToplam}TL")
-print(f"{kahveSayi} bardak kahve toplam {kahveFiyat}*{kahveSayi} = {kahveToplam}TL")
-print(f"Toplam tutar: {cayToplam} + {kahveToplam} = {total}TL ")
+print(f"ortalaman: {ortalama}")
