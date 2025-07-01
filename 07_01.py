c08c9db36f92d49dc67b9eee0079884f8b3c2959
SonGitBukucu <makaya2620@gmail.com>
2025-07-01 15:52:13 +0300

    15.52

--- 07_01.py
+++ 07_01.py
@@ -1,19 +1,6 @@
-pasta = int(input("Kaç kişi pasta yedi? : "))
-ekler = int(input("Kaç kişi ekler yedi? : "))
-baklava = int(input("Kaç kişi baklava yedi? : "))
-tatli =  pasta + ekler + baklava
+yas = int(input("yaşınızı girin: "))
 
-kandil = int(input("kaç kişi kandil simidi yedi? : "))
-sarma = int(input("kaç kişi sarma yedi? : "))
-kisir = int(input("kaç kişi kısır yedi? : "))
-tuzlu = kandil + sarma + kisir
-
-toplamYiyecek = tatli + tuzlu
-tatliYuzde = tatli / toplamYiyecek * 100
-tuzluYuzde = tuzlu / toplamYiyecek * 100
-baklavaYuzde = baklava / tatli * 100
-sarmaYuzde = sarma / tuzlu * 100
-
-print(f"tatlı-tuzlu yüzde oranları: {tatliYuzde} tatlı / {tuzluYuzde} tuzlu ({tatli} tatlı / {tuzlu} tuzlu)")
-print(f"tatlılar arasında baklavanın oranı yüzde {baklavaYuzde}")
-print(f"tuzlular arasında sarmanın yüzde oranı yüzde {sarmaYuzde}")
+if yas < 18:
+    print("ehliyet alamazsın")
+else :
+    print("ehliyet alabilirsin")
