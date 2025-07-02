cf51d01091ca6acb52cade45fa57a80cad4b125e
SonGitBukucu <makaya2620@gmail.com>
2025-07-02 20:27:40 +0300

    gün bitti notunu eklemeyi ve yeni güne transferi unutmuşum

--- 07_02.py
+++ 07_02.py
@@ -2,3 +2,5 @@ x = int(input("bir sayı girin: "))
 
 for i in range(0,x):
     print(i + 1, x)
+
+#gün bitti
---
+++ 07_03.py
@@ -0,0 +1,4 @@
+x = int(input("bir sayı girin: "))
+
+for i in range(0,x):
+    print(i + 1, x)
