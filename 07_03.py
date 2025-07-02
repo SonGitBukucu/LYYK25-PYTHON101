47d5b61f9d0a8816876c6f76fb5811ea4f70c5e7
SonGitBukucu <makaya2620@gmail.com>
2025-07-02 22:23:55 +0300

    ilk ödev

--- 07_03.py
+++ 07_03.py
@@ -1,4 +1,11 @@
+""" sıfır girilene kadar girilen değerleri toplayıp yazdır
 x = int(input("bir sayı girin: "))
 
-for i in range(0,x):
-    print(i + 1, x)
+a = x
+
+while x != 0:
+    x = int(input("bir sayı girin: "))
+    a += x
+if x == 0:
+    print(a)
+"""
