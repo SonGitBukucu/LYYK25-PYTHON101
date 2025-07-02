dc68875c6d33ab4c0d91c3f964934bb7b67a3f49
SonGitBukucu <makaya2620@gmail.com>
2025-07-02 14:06:30 +0300

    14.06 düzeltme

--- 07_02.py
+++ 07_02.py
@@ -1,7 +1,7 @@
 dersNot = int(input("not giriniz : "))
 devamsizlik = int(input("kaç gün devamsızlığınız var? : "))
 
-if devamsizlik < 10:
+if devamsizlik <= 10:
     if dersNot >= 85 and dersNot <= 100:
         print("Takdir belgesi aldınız")
     elif dersNot < 85 and dersNot >= 70:
