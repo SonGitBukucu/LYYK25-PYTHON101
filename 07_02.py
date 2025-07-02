237f90474beefd35a67c6a68ed535fab75fe45c2
SonGitBukucu <makaya2620@gmail.com>
2025-07-02 11:02:14 +0300

    11.02

--- 07_02.py
+++ 07_02.py
@@ -1,26 +1,6 @@
-fizik = int(input("Fizik ders notu: "))
-if fizik < 0:
-    print("Notlar 0'dan küçük olamaz")
-elif fizik > 100:
-    print("Notlar 100'den büyük olamaz")
-else:
-    kimya = int(input("Kimya ders notu: "))
-    if kimya < 0:
-        print("Notlar 0'dan küçük olamaz")
-    elif kimya > 100:
-        print("Notlar 100'den büyük olamaz")
-    else:
-        biyoloji = int(input("Biyoloji ders notu: "))
-        if biyoloji < 0:
-            print("Notlar 0'dan küçük olamaz")
-        elif biyoloji > 100:
-            print("Notlar 100'den büyük olamaz")
-        else:
-            ortalama = (fizik + kimya + biyoloji) / 3
-            print("Notlar geçerli.")
-            if ortalama < 50:
-                print("Sınıfta kaldınız.")
-            elif ortalama == 50:
-                print("Hediye kazandınız.")
-            else:
-                print("Sınıfı geçtiniz.") 
+isik = input("Işık ne renk? : ")
+
+if isik == "kırmızı":
+    print("dur")
+elif isik == "yeşil":
+    print("geç")
