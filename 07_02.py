43796ee068f3fc1ea72df215a32086c46b6a1022
SonGitBukucu <makaya2620@gmail.com>
2025-07-02 14:50:00 +0300

    14.50 dördüncü gün dosyası kaldırıldı

--- 07_02.py
+++ 07_02.py
@@ -1,12 +1,2 @@
-dersNot = int(input("not giriniz : "))
-devamsizlik = int(input("kaç gün devamsızlığınız var? : "))
-
-if devamsizlik < 10:
-    if dersNot >= 85 and dersNot <= 100:
-        print("Takdir belgesi aldınız.")
-    elif dersNot < 85 and dersNot >= 70:
-        print("Teşekkür belgesi aldınız.")
-    else:
-        print("Hiçbir belge alamadınız.")
-else:
-    print("Hiçbir belge alamadınız.")
+liste = ["turkiye", "almanya", "italya", "amerika", "ingiltere"]
+print(liste)
